c1e74480a479afd6681fa90f47fd3eeff1d56294
sjh83 <sjh908030@gmail.com>
2026-03-16 16:57:41 +0900

    feat: 상세 페이지 작업 임시 저장

---
+++ src/data/mockData.js
@@ -0,0 +1,20 @@
+export const mockProducts = [
+  {
+    id: 1,
+    title: 'RGB 게이밍 키보드',
+    category: 'keyboard',
+    price: 79000,
+    thumbnail: '상품이미지',
+    images: ['상세이미지1', '상세이미지2'],
+    description: '기계식 스위치를 적용한 RGB 게이밍 키보드',
+  },
+  {
+    id: 2,
+    title: 'RGB 게이밍 키보드 2',
+    category: 'keyboard',
+    price: 92400,
+    thumbnail: '상품이미지',
+    images: ['상세이미지3', '상세이미지4'],
+    description: '빠른 입력 반응과 몰입감 있는 조명 효과를 제공하는 텐키리스 게이밍 키보드.',
+  },
+];
